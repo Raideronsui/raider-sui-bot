114dd00f57425765417e28a5dd718884f182f70f
Raideronsui <onsuibluey@gmail.com>
2025-06-17 23:49:40 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
+import logging
 from dotenv import load_dotenv
-load_dotenv()
 
-# Load environment variables from .env file
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
